6b4f3c9cdda900091c59fd06aa3992a9c7823c34
Hien22012008 <xuanhiennguyen34@gmail.com>
2023-01-14 12:44:26 +0700

    myDog

---
+++ Bài về nhà - buổi 2/index.js
@@ -0,0 +1,13 @@
+var myDog = {
+    "name" : "Ngáo",
+    "legs" : 4,
+    "friends" : ["everything !"]
+}
+
+myDog.color = "Brown"
+
+delete myDog.friends
+
+myDog.name= "Husky"
+
+console.log(myDog);
