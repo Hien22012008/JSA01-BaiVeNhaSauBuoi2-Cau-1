b2e5775db4c1dcfaa0d58b59c884a57c6e6b852c
Hien22012008 <xuanhiennguyen34@gmail.com>
2023-02-05 14:47:14 +0700

    Buoi 4

--- Bài về nhà - buổi 2/index.js
+++ Bài về nhà - buổi 2/index.js
@@ -1,12 +1,16 @@
-var myDog = {
-    "name": "Ngáo",
-    "legs": 4,
-    "friends": ["everythings!"]
+// var myDog = {
+//     "name": "Ngáo",
+//     "legs": 4,
+//     "friends": ["everythings!"]
+// }
+
+// myDog.name = "Husky";
+
+// myDog.color = "Brown";
+
+// delete myDog.friends
+// console.log(myDog)
+var myfruit = [["Orange",15],["Grape",15],["WaterMelon",1]]
+for(var myfruit = 0 ; myfruit <= 3; myfruit++){
+    console.log( myfruit )
 }
-
-myDog.name = "Husky";
-
-myDog.color = "Brown";
-
-delete myDog.friends
-console.log(myDog)
