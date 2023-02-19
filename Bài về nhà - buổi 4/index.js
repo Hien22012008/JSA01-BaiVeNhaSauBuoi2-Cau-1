1a6e9705ffdd42c62e09ad521b5694e59f061360
Hien22012008 <xuanhiennguyen34@gmail.com>
2023-02-19 12:01:40 +0700

    Buoi 5

---
+++ Bài về nhà - buổi 4/index.js
@@ -0,0 +1,3 @@
+localStorage.setItem("người dùng","Hiển")
+localStorage.removeItem("người dùng")
+console.log(localStorage)
---
+++ bài về nhà buổi 3/index.js
@@ -0,0 +1,5 @@
+for (var a = 0 ; a <= 5; a++){
+for (var b=1 ; a < b-1; b++){
+    console.log("")
+}
+}
